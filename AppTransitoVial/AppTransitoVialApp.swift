82f075f1161a1609dedd8cefb5928a37a9e06149
OkapiGG <alancervantes800@gmail.com>
2026-03-04 14:44:16 -0600

    first commit

---
+++ AppTransitoVial/AppTransitoVialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTransitoVialApp.swift
+//  AppTransitoVial
+//
+//  Created by Alan Cervantes on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AppTransitoVialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
